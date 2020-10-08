cf0913f073f8ea12b4f5db39cdfdcf4461b544cd
tecunMartin <tecunmartin132@gmail.com>
2020-10-08 12:52:44 -0600

    mend

--- DBIN5BM.sql
+++ DBIN5BM.sql
@@ -26,14 +26,12 @@ create table Usuario (
 insert into Rol (codigo_rol, descripcion) value(1, 'Administrador');
 insert into Rol (codigo_rol, descripcion) value(2, 'Usuario');
 
-#==== Registros de Usuario
-insert into Usuario(codigo_Usuario, users, passwords, rols) value (1, 'tecunMartin', '1234', 1);
-insert into Usuario(codigo_Usuario, users, passwords, rols) value (2, 'aleCh', '1234', 2);
-insert into Usuario(codigo_Usuario, users, passwords, rols) value (3, 'manuelVega', '1234', 1);
-insert into Usuario(codigo_Usuario, users, passwords, rols) value (4, 'otherUser', '1234', 2);
-insert into Usuario(codigo_Usuario, users, passwords, rols) value (5, 'perezLopes', '1234', 1);
+#==== Registros de Login
+#insert into Usuario(codigo_Usuario, users, passwords, rols) value (1, 'tecunMartin', '1234', 1);
+#insert into Usuario(codigo_Usuario, users, passwords, rols) value (2, 'aleCh', '1234', 2);
 
-
-insert into persona(DPI,NombrePersona) value(201622849156,"Martin");
-insert into persona(DPI,NombrePersona) value(202015489120,"Alejandro");
-insert into persona(DPI,NombrePersona) value(405060107890,"Pedro");
+#insert into persona(DPI,NombrePersona) value(201622849156,"Martin");
+#insert into persona(DPI,NombrePersona) value(202015489120,"Alejandro");
+#insert into persona(DPI,NombrePersona) value(405060107890,"Pedro");
+#insert into persona(DPI,NombrePersona) value(504030201078,"Juan");
+#insert into persona(DPI,NombrePersona) value(123456789101,"Pablo");
